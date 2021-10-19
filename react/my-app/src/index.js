34e7ae6e28e27af286c0f88ff0c5332eb754d2e3
Nahid hossain <nahidhossain124@gmail.com>
2021-10-19 21:41:36 +0600

    first commit

---
+++ react/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Eblo</h1>
+    <h2>Nahud</h2>
+    <h1>allu</h1>
+    <h7>ey</h7>
+  </div>,
+  document.getElementById("root")
+);
